c9277f1baa6b4376bffc0a697093eeabd2e5a2e4
marataitester-blip <marat.aitester@gmail.com>
2026-03-23 12:58:38 +0300

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -84,7 +84,7 @@ export async function authenticateAction(prevState: any, formData: FormData) {
       httpOnly: true, // Защита от XSS атак
       secure: true,   // Только через HTTPS
       sameSite: 'strict', // Защита от CSRF
-      maxAge: 60 * 60 * 24 * 30, // 3 минуты
+      maxAge: 60 * 01 * 00 * 00, // 1 минута
       path: '/',
     });
     redirect('/'); // Успех -> на главную
